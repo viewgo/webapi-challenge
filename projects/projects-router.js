74903e8a4484291a81a5b52211ac439ee37888a5
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-06 10:58:05 -0600

    POST, DELETE, PUT added with validation

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -21,32 +21,88 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", validateProjectId, (req, res) => {
+  projects
+    .get(req.params.id)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      console.log(`error on GET /projects/${req.params.id}`, err);
+      res
+        .status(500)
+        .json({ error: "The project information could not be retrieved." });
+    });
+});
+
+router.post("/", validateProject, (req, res) => {
+  projects
+    .insert(req.body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      console.log(`error on POST /projects`, err);
+      res
+        .status(500)
+        .json({ error: "The project information could not be retrieved." });
+    });
+});
+
+router.delete("/:id", validateProjectId, (req, res) => {
     projects
-        .get(req.params.id)
+        .remove(req.params.id)
+        .then(response => {
+            res.status(200).json({message: "Project deleted successfully."});
+        })
+        .catch(err => {
+            console.log(`error on DELETE /projects/${req.params.id}`, err);
+            res
+              .status(500)
+              .json({ error: "The project information could not be deleted." });
+          });
+})
+
+router.put("/:id", validateProjectId, validateProject, (req, res) => {
+    projects
+        .update(req.params.id, req.body)
         .then(project => {
             res.status(200).json(project);
         })
+        .catch(err => {
+            console.log(`error on PUT /projects/${req.params.id}`, err);
+            res
+              .status(500)
+              .json({ error: "The project information could not be changed." });
+          });
 })
 
+
+
 //custom middleware
-function validateProjectId(req, res, next){
-    projects
-    .get(req.params.id)
-    .then(project => {
-        if(project){
-            next();
-        } else {
-            res.status(404).json({message: "The project with the specified ID does not exist."});
-        }
-    })
-    .catch(err => {
-        console.log(`error on GET /projects/:id`, err);
-        res
-          .status(500)
-          .json({ error: "The project information could not be retrieved." });
-      });
+function validateProjectId(req, res, next) {
+  projects.get(req.params.id).then(project => {
+    if (project) {
+      next();
+    } else {
+      res
+        .status(404)
+        .json({ message: "The project with the specified ID does not exist." });
+    }
+  });
 }
 
+function validateProject(req, res, next) {
+  if (!Object.keys(req.body).length > 0) {
+    res.status(400).json({ message: "missing project data" });
+  } else if (!req.body.name || !req.body.description) {
+    res
+      .status(400)
+      .json({ message: "missing required name and/or description field" });
+  } else {
+    console.log(req.body.name);
+    next();
+  }
+}
 
 //export
 module.exports = router;
