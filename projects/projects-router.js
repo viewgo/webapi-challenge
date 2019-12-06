dbfaa45bcbbc0259c2037afdb3218981a3c0bdad
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-06 10:33:43 -0600

    projects GET by id added with validation middleware

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -20,6 +20,33 @@ router.get("/", (req, res) => {
     });
 });
 
-//custom middleware
+router.get("/:id", validateProjectId, (req, res) => {
+    projects
+        .get(req.params.id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+})
 
+//custom middleware
+function validateProjectId(req, res, next){
+    projects
+    .get(req.params.id)
+    .then(project => {
+        if(project){
+            next();
+        } else {
+            res.status(404).json({message: "The project with the specified ID does not exist."});
+        }
+    })
+    .catch(err => {
+        console.log(`error on GET /projects/:id`, err);
+        res
+          .status(500)
+          .json({ error: "The project information could not be retrieved." });
+      });
+}
+
+
+//export
 module.exports = router;
